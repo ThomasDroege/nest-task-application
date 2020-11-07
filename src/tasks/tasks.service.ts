f186dbe307daeb739705ab2a3838463f39ad972e
thomasdroege <ThomasDroege@gmx.de>
2020-11-07 13:54:04 +0100

    added task model to service and controller

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from './task.model';
 
 // Service in general provides the business logic
 // The responsibility of Task service is e.g. to get all Tasks
 // The service will be injected into the controller, which responsibility is to manage the requests
 @Injectable()
 export class TasksService {
-    private tasks = [];
+    private tasks: Task[] = [];
 
-    getAllTasks() {
+    getAllTasks(): Task[] {
         return this.tasks;
     }
 }
