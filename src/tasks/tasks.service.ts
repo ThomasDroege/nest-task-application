3c3f2d9dd066b5aca7973426eea3c15b58d2175d
thomasdroege <ThomasDroege@gmx.de>
2020-11-07 13:46:44 +0100

    added getAllTasks method to task controller

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
 
+// Service in general provides the business logic
+// The responsibility of Task service is e.g. to get all Tasks
+// The service will be injected into the controller, which responsibility is to manage the requests
 @Injectable()
-export class TasksService {}
+export class TasksService {
+    private tasks = [];
+
+    getAllTasks() {
+        return this.tasks;
+    }
+}
