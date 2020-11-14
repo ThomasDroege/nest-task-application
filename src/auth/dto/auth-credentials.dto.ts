a6816ed8c0d5517b3fabc3f6097d66374d7e973b
thomasdroege <ThomasDroege@gmx.de>
2020-11-14 19:30:53 +0100

    added password requirement errormessage

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -10,6 +10,7 @@ export class AuthCredentialsDto {
     @IsString()
     @MinLength(8)
     @MaxLength(20)
-    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/)
+    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/,
+    {message: 'password too weak'})
     password: string;
 }
